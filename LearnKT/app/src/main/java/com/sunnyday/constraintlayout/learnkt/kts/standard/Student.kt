f26adedb424fe1548e8d2a59e851467ccc27b8d0
SunnyDay <7403365+sunnnydaydev@user.noreply.gitee.com>
2022-01-01 20:33:00 +0800

    modify add kt 标准函数

---
+++ LearnKT/app/src/main/java/com/sunnyday/constraintlayout/learnkt/kts/standard/Student.kt
@@ -0,0 +1,13 @@
+package com.sunnyday.constraintlayout.learnkt.kts.standard
+
+/**
+ * Create by SunnyDay on 20:20 2022/01/01
+ */
+
+class Student {
+
+    fun doHomework(){}
+
+    fun readBook(){}
+
+}
